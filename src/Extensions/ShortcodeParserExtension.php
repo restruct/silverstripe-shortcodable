78b562732fe4c165da83c06600cf5b867f32c6cd
Michael van Schaik <mic@restruct.nl>
2025-06-18 19:23:26 +0200

    Update ShortcodeParserExtension.php to prevent running on instances it shouldn't

--- src/Extensions/ShortcodeParserExtension.php
+++ src/Extensions/ShortcodeParserExtension.php
@@ -3,8 +3,11 @@
 namespace Shortcodable\Extensions;
 
 use Shortcodable\Shortcodable;
+use SilverStripe\Assets\Shortcodes\ImageShortcodeProvider;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Extension;
+use SilverStripe\Dev\Debug;
+use SilverStripe\Dev\DebugView;
 use SilverStripe\View\Parsers\ShortcodeParser;
 
 class ShortcodeParserExtension
@@ -13,37 +16,47 @@ class ShortcodeParserExtension
     // Check the shortcode wrapper element and wrap in wrap el if block type
     public function onBeforeParse(&$content)
     {
+        // Since we cannot read the 'identifier' (ShortcodeParser::$active_instance), instead we filter on registered
+        // shortcodes of the current instance to prevent running this on instances it shouldn't (eg 'regenerator');
         /** @var ShortcodeParser $parser */
         $parser = $this->owner;
+        $sc_parser_instance_shortcodes = array_keys($parser->getRegisteredShortcodes());
         $sc_block_wrapped_map = [];
         foreach(Shortcodable::get_shortcodable_sc_class_map() as $sc_tag => $sc_class){
-            if (Config::inst()->get($sc_class, 'shortcode_close_parent')) {
+            if (in_array($sc_tag, $sc_parser_instance_shortcodes) && Config::inst()->get($sc_class, 'shortcode_close_parent')) {
                 $sc_block_wrapped_map[] = $sc_tag;
             }
         }
-        if(!count($sc_block_wrapped_map)) return;
-
-        // assemble regex string and replace: https://regex101.com/r/FriwNk/1
-        // (appended .{0,10} to also select a part of the string directly following our schortcode tag)
+        if(!count($sc_block_wrapped_map)) {
+            return;
+        }
+        
+        // Assemble regex string: https://regex101.com/r/FriwNk/1
+        // ~> appended .{0,10} to also select a part of the string directly following our schortcode tag
         $regex = '<([a-z0-9]+)[^>]*?>.*?(\[(' . implode('|', $sc_block_wrapped_map) . ')[^]]*?\]([^[]*?\[/\3\])*).{0,10}';
         $content = preg_replace_callback(
             '%'.$regex.'%i',
             function ($matches) use ($sc_block_wrapped_map) {
                 $full_matched_string = $matches[0];
-//                [1]=>"p" <-- current wrapping element (to close & reopen)
-                $match_parent_block_el = $matches[1];
-//                [2]=>"[currentyear]" <-- full shortcode string (incl [sc attr="xy"]wrapped [/sc] verstions
-                $match_full_sc_string = $matches[2];
-//                [3]=> "currentyear" <-- shortcode 'tag'
-                $match_sc_tag = $matches[3];
+                $parent_el = $matches[1]; // [1]=>"p" <-- current wrapping element (to close & reopen)
+                $full_sc = $matches[2]; // [2]=>"[currentyear]" <-- full shortcode string (incl [sc attr="xy"]wrapped [/sc] verstions
+                $sc_tag = $matches[3]; // [3]=> "currentyear" <-- shortcode 'tag'
+                
+                // eg </p>[currentyear]<p>
+                $replacement_html = "</{$parent_el}>{$full_sc}<{$parent_el}>";
+                // replace (eg [currentyear] -> </p>[currentyear]<p>)
+                $return_str = str_replace($full_sc, $replacement_html, $full_matched_string);
 
-                // replace and 'clean up' - fix/remove '<br></p>' & <p></p> situations
-                $parent_closed_reopened_replacement = "</{$match_parent_block_el}>{$match_full_sc_string}<{$match_parent_block_el}>";
-                $return_str = str_replace($matches[2], $parent_closed_reopened_replacement, $full_matched_string);
-                $return_str = str_replace("<br>$parent_closed_reopened_replacement", $parent_closed_reopened_replacement, $return_str);
-                $return_str = str_replace("$match_full_sc_string<{$match_parent_block_el}></{$match_parent_block_el}>", $match_full_sc_string, $return_str);
-
-                return $return_str;
+                $replace_items = [
+                    // <br></p>[currentyear]<p> -> </p>[currentyear]<p> (removes <br> before)
+                    "<br>$replacement_html"                 => $replacement_html,
+                    // <p></p>[currentyear] -> [currentyear]
+                    "<{$parent_el}></{$parent_el}>$full_sc" => $full_sc,
+                    // [currentyear]<p></p> -> [currentyear]
+                    "$full_sc<{$parent_el}></{$parent_el}>" => $full_sc,
+                ];
+                
+                return str_replace(array_keys($replace_items), array_values($replace_items), $return_str);
             },
             (string) $content
         );
